3fa12d7740030e972d255eb78acc69a27d12460b
usernameapril <18mata.a@twain239.org>
2018-08-09 20:01:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+  def initialize (fido,snoopy,lassie)
+    @fido = fido
+    @snoopy = snoopy
+    @lassie = lassie
+  end
+  def fido
+    @fido
+  end
+ def snoopy
+   @snoopy
+ end
+def lassie
+  @lassie
+end
+dog_name=Dog.new
+
+
+   
